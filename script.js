d84271deabbfe3e66d282d0ef1944eaa9aaf0689
islamhack01 <iaubakirov503@gmail.com>
2023-06-18 19:09:54 +0600

    hello world

---
+++ script.js
@@ -0,0 +1,20 @@
+// let res = [];
+// for (let i = 1; i <= 7; i++) {
+//   res += "#";
+//   console.log(res);
+// }
+
+// Вам дан массив высчитать сумму до первого 0
+
+// function check(arr) {
+//     let answer = 0;
+//   arr = arr.reduce((acc, el) => {
+//     if (el === 0) {
+//       return (answer = acc);
+//     } else {
+//       return acc + el;
+//     }
+//   });
+//   console.log(answer);
+// }
+// check([12, 33, 43, 21, 0, 76, 43, 32]);
